e45bdce0506626bdf1ec114b6dd6d62f6c0b7c05
va-z <vasilizadorojnyuk@gmail.com>
2020-12-20 19:46:30 +0500

    feat: add UN country codes

--- src/js/data/_fetchData.js
+++ src/js/data/_fetchData.js
@@ -10,10 +10,10 @@ async function fetchData({
     fetch(CUMULATIVE_WORLD_BASE + CUMULATIVE_PARAM).then((res) => res.json()),
     fetch(LAST_COUNTRIES).then((res) => res.json()),
   ]);
-  const codes = dataArr[2]
+  const alpha2Codes = dataArr[2]
     .reduce((acc, { countryInfo: { iso2 } }) => `${acc},${iso2}`, '')
     .slice(1);
-  const url = `${CUMULATIVE_COUNTRIES_BASE}${codes}${CUMULATIVE_PARAM}`;
+  const url = `${CUMULATIVE_COUNTRIES_BASE}${alpha2Codes}${CUMULATIVE_PARAM}`;
   const cumulativeCountries = await fetch(url).then((res) => res.json());
 
   dataArr.push(cumulativeCountries);
