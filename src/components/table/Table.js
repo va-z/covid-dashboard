aff2131f7233eceaae16e62d4c18255f931560d6
va-z <vasilizadorojnyuk@gmail.com>
2020-12-22 15:59:04 +0500

    feat: add table

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,6 +1,7 @@
 import './Table.scss';
 import { TAGS, CLASSES } from '../../js/constants/index';
 import Element from '../_common/Element';
+import TableRow from '../_common/tableRow/TableRow';
 import FullscreenContainer from '../_common/fullscreenContainer/FullscreenContainer';
 import Toggle from '../_common/toggle/Toggle';
 
@@ -8,19 +9,30 @@ class Table extends FullscreenContainer {
   constructor() {
     super();
     this.addClasses(CLASSES.TABLE.TABLE);
+    this.statuses = ['Cases', 'Deaths', 'Recovered'];
+    this.rows = [];
 
-    const title = Element.createDOM({
+    this.title = Element.createDOM({
       tagName: TAGS.H2,
       className: CLASSES.TABLE.TABLE_TITLE,
       textContent: 'Global/Country',
     });
 
-    this.table = Element.createDOM({
+    const table = Element.createDOM({
       tagName: TAGS.UL,
       className: CLASSES.TABLE.TABLE_BLOCK,
     });
 
-    this.togglesContainer = Element.createDOM({
+    this.statuses.forEach((status) => {
+      this[`${status}Row`] = new TableRow({
+        status,
+        outerClasses: CLASSES.TABLE[`TABLE-ROW-${status.toUpperCase()}`],
+      });
+      this.rows.push(this[`${status}Row`]);
+      table.append(this[`${status}Row`].element);
+    });
+
+    const togglesContainer = Element.createDOM({
       className: CLASSES.STATIC.TOGGLES_CONTAINER,
     });
 
@@ -39,11 +51,15 @@ class Table extends FullscreenContainer {
       this.toggleAmount,
     ];
 
-    this.togglesContainer.append(
+    togglesContainer.append(
       this.togglePeriod.element,
       this.toggleAmount.element,
     );
-    this.element.append(title, this.table, this.togglesContainer);
+    this.element.append(
+      this.title,
+      table,
+      togglesContainer,
+    );
   }
 
   update({ data, state, change }) {
@@ -53,10 +69,16 @@ class Table extends FullscreenContainer {
       });
     }
 
-    const { name } = state;
+    const { name, figure, amount } = state;
     const src = data.find((obj) => obj.name === name);
+    this.title.textContent = name;
 
-    console.log(src);
+    this.statuses.forEach((status, ind) => {
+      const key = figure + status + (amount === 'abs' ? '' : '100k');
+      const val = src[key];
+
+      this.rows[ind].update(val);
+    });
   }
 }
 
